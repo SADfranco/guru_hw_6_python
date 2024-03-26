a8cddd38005c446ae4ccf949adf157ebd5238e7f
Andrei Shabaev <shabaevandrew@gmail.com>
2024-03-26 20:33:04 +0300

    changed dase on commets

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -5,10 +5,10 @@ def test_dark_theme_by_time():
     """
     Протестируйте правильность переключения темной темы на сайте в зависимости от времени
     """
-    current_time = time(hour=23)
+    current_time = time(hour=5)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    if 22 <= current_time.hour or current_time.hour < 6:
+    if current_time.hour in [0,1,2,3,4,5,22,23]:
         is_dark_theme = True
     else:
         is_dark_theme = False
@@ -25,14 +25,14 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
-    current_time = time(hour=16)
-    dark_theme_enabled_by_user = True
+    current_time = time(hour=4)
+    dark_theme_enabled_by_user = False
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
     if dark_theme_enabled_by_user:
         is_dark_theme = True
-    elif dark_theme_enabled_by_user == None and 22 <= current_time.hour or current_time.hour < 6:
+    elif dark_theme_enabled_by_user == None and current_time.hour in [0,1,2,3,4,5,22,23]:
         is_dark_theme = True
     else:
         is_dark_theme = False
